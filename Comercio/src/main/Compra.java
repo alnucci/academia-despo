bc41b370c986a0f13b6a4417c7db5038a5c3dd9b
aluno <aluno@10.32.16.20>
2018-09-05 10:31:54 -0300

    Unificação dos projetos

---
+++ Comercio/src/main/Compra.java
@@ -0,0 +1,35 @@
+package br.gov.serpro.comercio;
+public class Compra {
+	public static void main(String[] args){
+
+    int valorTotal;
+    int numeroParcelas;
+
+    // a vista
+    public Compra(int valor) {
+        valorTotal = valor;
+        numeroParcelas = 1;
+    }
+
+
+    //parcelado
+    public Compra(int qtdParcelas, int valorParcela) {
+        numeroParcelas = qtdParcelas;
+        valorTotal = valorParcela * qtdParcelas;
+
+    }
+
+    public int getValorTotal() {
+        return valorTotal;
+    }
+
+    public int getNumeroParcelas(){
+        return numeroParcelas;
+    }
+
+    public int getValorParcelas(){
+        return valorTotal/numeroParcelas;
+    }
+	}
+}
+	
---
+++ Comercio/src/test/TesteCompra.java
@@ -0,0 +1,27 @@
+package br.gov.serpro.comercio;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class TesteCompra  {
+    @Test
+public void compraAVista(){
+        Compra c = new Compra(500);
+        assertEquals(1, c.getNumeroParcelas());
+        assertEquals(500, c.getValorTotal());
+        assertEquals(500, c.getValorParcelas());
+
+    }
+
+    @Test
+    public void compraParcelado(){
+        Compra c = new Compra(4,250);
+        assertEquals(4, c.getNumeroParcelas());
+        assertEquals(1000, c.getValorTotal());
+        assertEquals(250, c.getValorParcelas());
+
+    }
+
+
+}
