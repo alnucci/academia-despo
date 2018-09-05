23615bb9c4f47199e1c4bf4ea0d3389f4250133f
aluno <aluno@10.32.16.20>
2018-09-05 09:32:06 -0300

    Maven

--- CaculadoraIMC/src/Paciente.java
+++ CaculadoraIMC/src/Paciente.java
@@ -1,19 +1,25 @@
 public class Paciente {
- double peso;
- double altura;
+ private double peso;
+ private double altura;
 
     public double getPeso() {
         return peso;
     }
 
-    public double getAltura() {
-        return altura;
+    public Paciente(double peso, double altura) {
+     this.peso = peso;
+     this.altura = altura;
+     
     }
-    class calcularIMC {
+    
+    
+    
+    
+    public double calcularIMC {
         public static void main ( String [] args ) {
             double ResultadoIMC = this.peso/(this.altura * this.altura);
             if( ResultadoIMC &gt 40 )
-            System.out.println("Obesidade mórbida");
+            System.out.println("Obesidade morbida");
       else if (ResultadoIMC &gt 35)
             System.out.println("Obesidade grau II");
       else if (ResultadoIMC &gt 30)
