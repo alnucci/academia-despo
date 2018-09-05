aec68ca47c34dc390200800b77dd68a82393f921
aluno <aluno@10.32.16.20>
2018-09-05 10:17:37 -0300

    COMPLEMENTO

--- CaculadoraIMC/src/Paciente.java
+++ CaculadoraIMC/src/Paciente.java
@@ -1,47 +1,36 @@
 public class Paciente {
- private double peso;
- private double altura;
+	private double peso;
+	private double altura;
 
-    
-    public Paciente(double peso, double altura) {
-     this.peso = peso;
-     this.altura = altura;
-     
-    }
-    
-    
-    
-    
-    public double calcularIMC() {
-        return this.peso / (this.altura * this.altura);
-    }
-     
-    public String diagnostico() {
-    	double ResultadoIMC = calcularIMC();
-    	
-            if (ResultadoIMC > 40 ) {
-            return "Obesidade morbida" ;
-            }
-            else if (ResultadoIMC > 35) {
-            return "Obesidade grau II";
-            }
-            else if (ResultadoIMC > 30) {
-            return "Obesidade grau I";
-            }
-        else if (ResultadoIMC > 25) {
-            return "Sobrepeso";
-        }
-      else if (ResultadoIMC > 18.5) {
-            return "Peso normal";
-      }
-      else if (ResultadoIMC > 17) {
-            return "Baixo peso";
-      }
-      else if (ResultadoIMC > 16) {
-            return "Baixo peso grave";
-      }
-      else
-            return "Baixo peso muito grave";
-        }
-   
+	public Paciente(double peso, double altura) {
+		this.peso = peso;
+		this.altura = altura;
+
+	}
+
+	public double calcularIMC() {
+		return this.peso / (this.altura * this.altura);
+	}
+
+	public String diagnostico() {
+		double ResultadoIMC = calcularIMC();
+
+		if (ResultadoIMC > 40) {
+			return "Obesidade morbida";
+		} else if (ResultadoIMC > 35) {
+			return "Obesidade grau II";
+		} else if (ResultadoIMC > 30) {
+			return "Obesidade grau I";
+		} else if (ResultadoIMC > 25) {
+			return "Sobrepeso";
+		} else if (ResultadoIMC > 18.5) {
+			return "Peso normal";
+		} else if (ResultadoIMC > 17) {
+			return "Baixo peso";
+		} else if (ResultadoIMC > 16) {
+			return "Baixo peso grave";
+		} else
+			return "Baixo peso muito grave";
+	}
+
 }
--- CaculadoraIMC/src/Principal.java
+++ CaculadoraIMC/src/Principal.java
@@ -2,15 +2,32 @@ public class Principal {
 
 	public static void main(String[] args) {
 
-		Paciente pacienteA = new Paciente(63,1.75);
+		Paciente pacienteA = new Paciente(160,1.75); //OM
 		
-		Paciente pacienteB = new Paciente(74,1.56 );
+		Paciente pacienteB = new Paciente(94,1.56 ); //OGII
 		
-		Paciente pacienteC = new Paciente(46,1.34 );
+		Paciente pacienteC = new Paciente(74,1.56 ); //OGI
 		
-		System.out.println("Indice de massa corporal (IMC) do paciente A: " + pacienteA.diagnostico()+ "\n" );
+		Paciente pacienteD = new Paciente(46,1.34 ); //S
+		
+	    Paciente pacienteE = new Paciente(63,1.75);  //NORMAL
+		
+		Paciente pacienteF = new Paciente(56,1.74 ); //BP
+		
+		Paciente pacienteG = new Paciente(50,1.74 ); //BPG
+		
+		Paciente pacienteH = new Paciente(46,1.84 ); //BPMG
+	
+		
+		System.out.println("Indice de massa corporal (IMC) do paciente A: " + pacienteA.diagnostico() + "\n" );
 		System.out.println("Indice de massa corporal (IMC) do paciente B: " + pacienteB.diagnostico() + "\n");
-		System.out.println("Indice de massa corporal (IMC) do paciente C: " + pacienteB.diagnostico() + "\n");
+		System.out.println("Indice de massa corporal (IMC) do paciente C: " + pacienteC.diagnostico() + "\n");
+		System.out.println("Indice de massa corporal (IMC) do paciente D: " + pacienteD.diagnostico() + "\n");
+
+		System.out.println("Indice de massa corporal (IMC) do paciente E: " + pacienteE.diagnostico() + "\n" );
+		System.out.println("Indice de massa corporal (IMC) do paciente F: " + pacienteF.diagnostico() + "\n");
+		System.out.println("Indice de massa corporal (IMC) do paciente G: " + pacienteG.diagnostico() + "\n");
+		System.out.println("Indice de massa corporal (IMC) do paciente H: " + pacienteH.diagnostico() + "\n");
 
 }
 }
