5d89b12a46ef08b92ce5946e4d4a48d106b29762
aluno <aluno@10.32.16.20>
2018-09-10 11:00:30 -0300

    Exercicio

--- CalculaMetodos/src/Calcular.java
+++ CalculaMetodos/src/Calcular.java
@@ -1,4 +1,16 @@
 
 public class Calcular {
-
+	static int valor1 = 0;
+	static int valor2 = 0;
+	
+	void comparar() {
+		if (valor1 > valor2) {
+		maior = valor1;
+		maior = valor2;
+	}
+	
+	
+	void imprimir() {
+		System.out.println("O somador "+nome+ ": instancia="+valorInstancia+" e estatica="+valorEstatica );
+	}
 }
