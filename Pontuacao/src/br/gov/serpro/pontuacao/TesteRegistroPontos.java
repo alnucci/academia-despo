55a112bcdaa90b31d456f7b8c6094d1d38a95bb4
Andre Nucci <alnucci@gmail.com>
2018-09-11 23:21:59 -0300

    versao 2

--- Pontuacao/src/br/gov/serpro/pontuacao/TesteRegistroPontos.java
+++ Pontuacao/src/br/gov/serpro/pontuacao/TesteRegistroPontos.java
@@ -9,11 +9,41 @@ import static org.junit.Assert.*;
 public class TesteRegistroPontos {
     @Test
     public void pontosCriarTopicos(){
-        br.gov.serpro.pontuacao.Usuario u = new Usuario()();
+        br.gov.serpro.pontuacao.Usuario u = new Usuario();
         u.nome = "Guerra";
         br.gov.serpro.pontuacao.CalculadoraBonus cb = new CalculadoraBonus();
         br.gov.serpro.pontuacao.RegistroPontos rp = new RegistroPontos();
         rp.criarUmTopico(u);
         assertEquals(u.pontos, 5);
+    }
+    @Test
+    public void pontosCriarTopicosVIP(){
+        br.gov.serpro.pontuacao.Usuario u = new Usuario();
+        u.nome = "Guerra";
+        br.gov.serpro.pontuacao.CalculadoraBonus cb = new CalculadoraBonus();
+        br.gov.serpro.pontuacao.RegistroPontos rp = new RegistroPontos();
+        rp.criarUmTopico(u);
+        assertEquals(u.pontos, 25);
+    }
+    @Test
+    public void pontosCriarTopicosBonusdoDia(){
+        br.gov.serpro.pontuacao.Usuario u = new Usuario();
+        u.nome = "Guerra";
+        br.gov.serpro.pontuacao.CalculadoraBonus cb = new CalculadoraBonus();
+        cb.bonusDoDia = 3;
+        br.gov.serpro.pontuacao.RegistroPontos rp = new RegistroPontos();
+        rp.criarUmTopico(u);
+        assertEquals(u.pontos, 15);
+    }
+    @Test
+    public void pontosCriarTopicosVIPBonusdoDia(){
+        br.gov.serpro.pontuacao.Usuario u = new Usuario();
+        u.nome = "Guerra";
+        u.vip - true;
+        br.gov.serpro.pontuacao.CalculadoraBonus cb = new CalculadoraBonus();
+        cb.bonusDoDia = 2;
+        br.gov.serpro.pontuacao.RegistroPontos rp = new RegistroPontos();
+        rp.criarUmTopico(u);
+        assertEquals(u.pontos, 50);
     }
 }
