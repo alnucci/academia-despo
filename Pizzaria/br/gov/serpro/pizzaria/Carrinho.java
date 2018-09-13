574f759abe046ee7e781a236a1f75a75aaa763cf
Andre Luis Nucci <andre.nucci@serpro.gov.br>
2018-09-13 15:27:13 -0300

    atualização

--- Pizzaria/br/gov/serpro/pizzaria/Carrinho.java
+++
@@ -1,5 +0,0 @@
-package br.gov.serpro.pizzaria;
-
-public class Carrinho {
-    // Calcula o valor total da compra com as pizzas e a entrega
-}
--- Pizzaria/br/gov/serpro/pizzaria/Entrega.java
+++
@@ -1,6 +0,0 @@
-package br.gov.serpro.pizzaria;
-
-public class Entrega {
- // Calcula a entrega baseado no dia da semana e na distância
-
-}
