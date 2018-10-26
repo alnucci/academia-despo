9119623a1919ea4f5d3cfa9fb4c744e8c3b9cad5
Andre Luis Nucci <andre.nucci@serpro.gov.br>
2018-10-26 19:00:15 -0300

    Metodo Acesso Data

---
+++ MetodosAcessoData/test/br/gov/serpro/academia/metodoacessodata/PessoaTest.java
@@ -0,0 +1,21 @@
+package br.gov.serpro.academia.metodoacessodata;
+
+import org.junit.Test;
+
+/**
+ * Created by 11315867869 on 26/10/18.
+ */
+public class PessoaTest {
+
+    @Test
+    public void testar_deveria_retornar_signo_aquario();
+
+
+    @Test
+    public void deveria_calcular_idade_pessoa();
+
+
+    @Test
+    public void testar_deveria_retornar_signo_cancer();
+
+}
