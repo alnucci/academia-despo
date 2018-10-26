724e54a4cfc3b1c5684c597286cb3aaf5c8cdcc1
aluno <aluno@10.32.16.20>
2018-10-26 10:55:45 -0300

    Metodos de acesso data

---
+++ MetodosAcessoData/src/br/gov/serpro/academia/metodoacessodata/Pessoa.java
@@ -0,0 +1,21 @@
+package br.gov.serpro.academia.metodoacessodata;
+
+import java.time.LocalDate;
+
+public class Pessoa {
+
+	private LocalDate dataDeNascimento;
+	
+	public Pessoa() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public LocalDate getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+
+	public void setDataDeNascimento(LocalDate dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+
+}
