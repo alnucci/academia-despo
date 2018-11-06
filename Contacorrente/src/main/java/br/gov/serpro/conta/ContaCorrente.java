8c0fc9fee7feb18114880fc50d7c8630132cef2a
Andre Luis Nucci <andre.nucci@serpro.gov.br>
2018-11-06 17:59:25 -0200

    Conta corrente 4

--- Contacorrente/src/main/java/br/gov/serpro/conta/ContaCorrente.java
+++ Contacorrente/src/main/java/br/gov/serpro/conta/ContaCorrente.java
@@ -4,7 +4,7 @@ package br.gov.serpro.conta;
  * Created by 11315867869 on 06/11/18.
  */
 public interface ContaCorrente {
-    void depositar(double v);
+    void depositar(double valor);
 
     double obterSaldo();
 
